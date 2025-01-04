b129f369b5f1e4f1a5aebe7479b17213443f0743
onesunny2 <ippne996@gmail.com>
2025-01-05 03:53:55 +0900

    세번째 쇼핑 탭바 오토레이아웃 잡고 로직 파일과 연결 완료

---
+++ Travel_Project/TableViewCell/ShoppingTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingTableViewCell.swift
+//  Travel_Project
+//
+//  Created by Lee Wonsun on 1/5/25.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    @IBOutlet var checkboxButton: UIButton!
+    @IBOutlet var favoriteButton: UIButton!
+    @IBOutlet var itemLabel: UILabel!
+    @IBOutlet var backgroundImageView: UIImageView!
+    
+
+}
---
+++ Travel_Project/TableViewController/ShoppingTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ShoppingTableViewController.swift
+//  Travel_Project
+//
+//  Created by Lee Wonsun on 1/5/25.
+//
+
+import UIKit
+
+class ShoppingTableViewController: UITableViewController {
+
+    @IBOutlet var textfieldBgImageView: UIImageView!
+    @IBOutlet var addButton: UIButton!
+    @IBOutlet var shoppingTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
